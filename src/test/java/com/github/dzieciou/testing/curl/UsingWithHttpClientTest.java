52131bb54680963f3567513d305c375a2150894b
Maciej Gawinecki <mgawinecki@gmail.com>
2019-01-02 20:36:00 +0100

    [#17] Fix recognizing HTTPS protocol for Apachae HTTP Client >= 4.3.

--- src/test/java/com/github/dzieciou/testing/curl/UsingWithHttpClientTest.java
+++ src/test/java/com/github/dzieciou/testing/curl/UsingWithHttpClientTest.java
@@ -1,6 +1,10 @@
 package com.github.dzieciou.testing.curl;
 
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import com.github.valfirst.slf4jtest.TestLoggerFactory;
 import java.io.IOException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -11,13 +15,28 @@ public class UsingWithHttpClientTest {
 
   private static HttpClient createHttpClient() {
     return HttpClientBuilder.create()
-        .addInterceptorFirst(new CurlLoggingInterceptor(Options.builder().build()))
+        .addInterceptorFirst(new CurlLoggingInterceptor(Options.builder()
+            .targetPlatform(Platform.UNIX) // TO ease verifying output curl
+            .build()))
         .build();
   }
 
   @Test(groups = "end-to-end-samples")
-  public void test() throws IOException {
+  public void testHttp() throws IOException {
+    TestLoggerFactory.clearAll();
     HttpGet getRequest = new HttpGet("http://google.com");
     createHttpClient().execute(getRequest);
+    assertThat(TestLoggerFactory.getAllLoggingEvents().get(0).getMessage(),
+        equalTo("curl 'http://google.com/' --compressed --insecure --verbose"));
   }
+
+  @Test(groups = "end-to-end-samples")
+  public void testHttps() throws IOException {
+    TestLoggerFactory.clearAll();
+    HttpGet getRequest = new HttpGet("https://google.com");
+    createHttpClient().execute(getRequest);
+    assertThat(TestLoggerFactory.getAllLoggingEvents().get(0).getMessage(),
+        equalTo("curl 'https://google.com/' --compressed --insecure --verbose"));
+  }
+
 }
